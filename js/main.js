ce421b4ee834baf000749793249e9929987c7a99
Ben Coleman <benc.uk@gmail.com>
2021-08-27 18:05:07 +0100

    ✨ More arp goodness

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 // HTML nodes
 let inputDeviceSelect = null
 let outputDeviceSelect = null
+let inputChannelSelect = null
+let outputChannelSelect = null
+let modeSelect = null
+let rangeSelect = null
+let clockModeSelect = null
+let divSelect = null
+
 let tempoSlider = null
 let notesDiv = null
 
@@ -10,21 +17,16 @@ let outputDevice = null
 
 // Core
 let ticks = 0
-let steps = 0
+let step = 0
 
 // Other shit
 import ClockTicker from './clock-ticker.js'
 let tempo = 120
-let CLOCK_EXTERNAL = 0
-let CLOCK_INTERNAL = 1
-let CLOCK_INTERNAL_SEND = 2
-let clockMode = CLOCK_EXTERNAL
 let internalClock = null
 
 // MIDI input and output
-let inputChannel = 3
-let outputChannel = 2
 let heldNotes = []
+let heldNotesSorted = []
 
 // =============================================================================
 // The main entry point is here, after loading the document / page
@@ -34,19 +36,35 @@ window.addEventListener('load', async () => {
   inputDeviceSelect.addEventListener('change', deviceSelected)
   outputDeviceSelect = document.getElementById('outputDeviceSel')
   outputDeviceSelect.addEventListener('change', deviceSelected)
+  inputChannelSelect = document.getElementById('inputChannelSel')
+  inputChannelSelect.addEventListener('change', initalizeArp)
+  outputChannelSelect = document.getElementById('outputChannelSel')
+  outputChannelSelect.addEventListener('change', initalizeArp)
+
+  modeSelect = document.getElementById('modeSel')
+  divSelect = document.getElementById('divSel')
+  rangeSelect = document.getElementById('rangeSel')
+  clockModeSelect = document.getElementById('clockModeSel')
+  clockModeSelect.addEventListener('change', initalizeArp)
+
   tempoSlider = document.getElementById('tempoSlider')
   notesDiv = document.getElementById('inputNotes')
 
+  for (let c = 1; c <= 16; c++) {
+    let option = document.createElement('option')
+    option.innerHTML = c
+    option.value = c
+    inputChannelSelect.appendChild(option)
+    option = document.createElement('option')
+    option.innerHTML = c
+    option.value = c
+    outputChannelSelect.appendChild(option)
+  }
+
   tempoSlider.addEventListener('change', (e) => {
     tempo = tempoSlider.value
-    document.getElementById('tempoSliderLabel').innerHTML = `${tempo} BPM`
-    startArp()
-  })
-
-  document.getElementById('modeSel').addEventListener('change', (e) => {
-    clockMode = e.target.value
-    console.log(`clockMode: ${clockMode}`)
-    startArp()
+    document.getElementById('tempoSliderLabel').innerHTML = `Tempo: ${tempo} BPM`
+    initalizeArp()
   })
 
   WebMidi.enable(function (err) {
@@ -83,9 +101,9 @@ function initialize() {
   }
 
   // !!! TEST HARNESS CODE !!!
-  inputDevice = WebMidi.inputs[0]
-  outputDevice = WebMidi.outputs[0]
-  startArp()
+  // inputDevice = WebMidi.inputs[0]
+  // outputDevice = WebMidi.outputs[0]
+  // startArp()
 }
 
 function deviceSelected(evt) {
@@ -95,13 +113,15 @@ function deviceSelected(evt) {
   if (evt.target.id == 'outputDeviceSel') outputDevice = WebMidi.getOutputById(evt.target.value)
 
   if (inputDevice && outputDevice) {
-    startArp()
+    initalizeArp()
   }
 }
 
-function startArp() {
-  heldNotes = []
-  console.log(`###----- startArp ${clockMode}`)
+function initalizeArp() {
+  if (!inputDevice || !outputDevice) return
+  let clockMode = clockModeSelect.value
+
+  console.log(`### initalizeArp ${clockMode} ${tempo}`)
   inputDevice.removeListener('clock', 'all')
   inputDevice.removeListener('noteon', 'all')
   inputDevice.removeListener('noteoff', 'all')
@@ -109,25 +129,24 @@ function startArp() {
     internalClock.removeEventListener('tick', handleTick)
     internalClock.removeEventListener('tick', handleTick)
   }
-  tempoSlider.style.display = 'none'
+  tempoSlider.disabled = true
 
-  inputDevice.addListener('noteon', inputChannel, (e) => {
+  inputDevice.addListener('noteon', inputChannelSelect.value, (e) => {
     addNote(e.note)
   })
 
-  inputDevice.addListener('noteoff', inputChannel, (e) => {
+  inputDevice.addListener('noteoff', inputChannelSelect.value, (e) => {
     removeNote(e.note)
   })
 
-  if (clockMode == CLOCK_EXTERNAL) {
+  if (clockMode == 'CLOCK_EXTERNAL') {
     console.log('EXTERNAL CLOCK')
     inputDevice.addListener('clock', 'all', handleTick)
   }
 
-  if (clockMode == CLOCK_INTERNAL || clockMode == CLOCK_INTERNAL_SEND) {
+  if (clockMode == 'CLOCK_INTERNAL' || clockMode == 'CLOCK_INTERNAL_SEND') {
+    tempoSlider.disabled = false
     console.log('CLOCK_INTERNAL')
-    tempoSlider.style.display = 'inline'
-    document.getElementById('tempoSliderLabel').innerHTML = `${tempo} BPM`
     internalClock = new ClockTicker(tempo)
     internalClock.addEventListener('tick', handleTick)
   }
@@ -135,9 +154,13 @@ function startArp() {
 
 function addNote(note) {
   heldNotes.push(note)
-  var e = document.createElement('h2')
+  heldNotesSorted.push(note)
+  heldNotesSorted.sort((a, b) => a.number - b.number)
+  console.log(JSON.stringify(heldNotesSorted))
+
+  var e = document.createElement('div')
   e.innerHTML = `${note.name}${note.octave}`
-  e.style.color = '#338833'
+  e.className = 'note'
   e.id = `note_${note.number}`
   notesDiv.appendChild(e)
 }
@@ -149,26 +172,59 @@ function removeNote(note) {
       break
     }
   }
-  document.getElementById(`note_${note.number}`).remove()
+  for (let i = 0; i < heldNotesSorted.length; i++) {
+    if (heldNotesSorted[i].number == note.number) {
+      heldNotesSorted.splice(i, 1)
+      break
+    }
+  }
+  try {
+    document.getElementById(`note_${note.number}`).remove()
+  } catch (e) {}
+  console.log(JSON.stringify(heldNotesSorted))
 }
 
 // The whole arp logic is here, called on every MIDI clock tick
 function handleTick() {
   ticks++
-  let interval = 12
+  let interval = divSelect.value
+  let mode = modeSelect.value
 
-  if (clockMode == CLOCK_INTERNAL_SEND) {
+  if (clockModeSelect.value == 'CLOCK_INTERNAL_SEND') {
     outputDevice.sendClock()
   }
 
   if (ticks >= interval) {
     ticks = 0
-    steps++
+    if (mode == 'UP' || mode == 'PLAYED') {
+      step++
+      if (step >= heldNotes.length) {
+        step = 0
+      }
+    }
+    if (mode == 'DOWN') {
+      step--
+      if (step < 0) {
+        step = heldNotes.length - 1
+      }
+    }
+    if (mode == 'RANDOM') {
+      step = Math.floor(Math.random() * heldNotes.length)
+    }
 
     if (heldNotes.length <= 0) return
-    let noteIndex = steps % heldNotes.length
-    let note = heldNotes[noteIndex]
+    if (heldNotesSorted.length <= 0) return
+
+    let note
+
+    if (mode == 'PLAYED') {
+      note = heldNotes[step]
+    } else {
+      note = heldNotesSorted[step]
+    }
+
     if (!note) return
-    outputDevice.playNote(note.number, outputChannel, { velocity: 0.7, duration: 50 })
+    console.log(`STEP:${step} = ${note.number}`)
+    outputDevice.playNote(note.number, outputChannelSelect.value, { velocity: 0.7, duration: 50 })
   }
 }
